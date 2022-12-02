ee658565708b2cfdf747f61e22cf22aed58fad93
anilsuwal <anilsuwal58@gmail.com>
2022-12-02 11:25:56 +0545

    Added Props and Emits

--- chapter8 component communication/cmp-communication-01-starting-setup/src/components/FriendContact.vue
+++ chapter8 component communication/cmp-communication-01-starting-setup/src/components/FriendContact.vue
@@ -1,15 +1,16 @@
 <template>
   <li>
-    <h2>{{ friend.name }}</h2>
+    <h2>{{ name }} {{ isFavorite ? '(Favorite)' : ''}}</h2>
+    <button @click="toggleFavorite">Toggle Favorite</button>
     <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
     <ul v-if="detailsAreVisible">
       <li>
         <strong>Phone:</strong>
-        {{ friend.phone }}
+        {{ phoneNumber }}
       </li>
       <li>
         <strong>Email:</strong>
-        {{ friend.email }}
+        {{ emailAddress }}
       </li>
     </ul>
   </li>
@@ -17,20 +18,60 @@
 
 <script>
 export default {
+  // props: [
+  //   'name',
+  //   'phoneNumber',
+  //   'emailAddress'
+  // ],
+  props: {
+    id: {
+      name: String,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    phoneNumber: {
+      type: String,
+      required: true
+    },
+    emailAddress: {
+      type: String,
+      required: true
+    },
+    isFavorite: {
+      type: Boolean,
+      required: false,
+      default: false,
+      // validator: function(value) {
+      //   return value === '1' || value === '0';
+      // }
+    }
+  },
+  emits: ['toggle-favorite'],
+  // emits: {
+  //   'togle-favorite': function(id) {
+  //     if(id) {
+  //       return true;
+  //     }else {
+  //       console.warn("Id is missing!");
+  //       return false;
+  //     }
+  //   }
+  // },
   data() {
     return {
-      detailsAreVisible: false,
-      friend: {
-        id: "manuel",
-        name: "Manuel Lorenz",
-        phone: "0123 45678 90",
-        email: "manuel@localhost.com",
-      },
+      detailsAreVisible: false, 
     };
   },
   methods: {
     toggleDetails() {
       this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFavorite() {
+      // this.friendIsFavorite = !this.friendIsFavorite;
+      this.$emit('toggle-favorite', this.id);
     }
   }
 };
